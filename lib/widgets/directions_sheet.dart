751d0bb559bb7257aa1dea40d2e9e73d5ee9e649
Ishan Kumar <littleishan@gmail.com>
2025-08-25 12:10:35 -0400

    added back loading screen i accidentally deleted

--- lib/widgets/directions_sheet.dart
+++ lib/widgets/directions_sheet.dart
@@ -98,10 +98,174 @@ class _DirectionsSheetState extends State<DirectionsSheet> {
         future: _listOfJourneys,
         builder: (context, journeyload) {
           if (journeyload.connectionState == ConnectionState.waiting) {
-            return SizedBox(
-              width: 20,
-              height: 20,
-              child: CircularProgressIndicator(strokeWidth: 2),
+
+            // If loading, return empty sheet...
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(95, 187, 187, 187), 
+                        spreadRadius: 2, 
+                        blurRadius: 6, 
+                        offset: Offset(0, 3), 
+                      ),
+                    ],
+                  ),
+
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.my_location,
+                              size: 25,
+                            ),
+
+                            SizedBox(width: 12,),
+
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () { 
+
+                                  showModalBottomSheet(
+                                    context: context,
+                                    isScrollControlled: true,
+                                    backgroundColor: Colors.transparent,
+                                    builder: (BuildContext context) {
+                                      return SearchSheet(
+                                        onSearch: (Location location, isStop, id) {
+                                          final searchCoordinates = location.latlng;
+                                          // null-proofing
+                                          if (searchCoordinates != null) {
+                                            Navigator.pop(context);
+                                            widget.onChangeSelection(location, true);
+                                          } else {
+                                            print("Error: The selected location '${location.name}' has no coordinates.");
+                                          }
+                                        },
+                                      );
+                                    },
+                                  );
+                                },
+                                child: Container(
+                                  alignment: Alignment.centerLeft,
+                                  height: 30,
+                                  decoration: BoxDecoration(
+                                    color: Color.fromARGB(255, 235, 235, 235),
+                                    borderRadius: BorderRadius.all(Radius.circular(10)
+                                    ),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 10, right: 10),
+                                    child: Text(
+                                      widget.originName,
+                                      style:  TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w400,
+                                        height: 0
+                                      ),
+                                      overflow: TextOverflow.ellipsis
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+
+                        SizedBox(height: 10,),
+                    
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              size: 25,
+                            ),
+
+                            SizedBox(width: 12,),
+
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () { 
+                                  showModalBottomSheet(
+                                    context: context,
+                                    isScrollControlled: true,
+                                    backgroundColor: Colors.transparent,
+                                    builder: (BuildContext context) {
+                                      return SearchSheet(
+                                        onSearch: (Location location, isStop, id) {
+                                          final searchCoordinates = location.latlng;
+                                          // null-proofing
+                                          if (searchCoordinates != null) {
+                                            Navigator.pop(context);
+                                            widget.onChangeSelection(location, false);
+                                          } else {
+                                            print("Error: The selected location '${location.name}' has no coordinates.");
+                                          }
+                                        },
+                                      );
+                                    },
+                                  );
+                                },
+                                child: Container(
+                                  alignment: Alignment.centerLeft,
+                                  height: 30,
+                                  decoration: BoxDecoration(
+                                    color: Color.fromARGB(255, 235, 235, 235),
+                                    borderRadius: BorderRadius.all(Radius.circular(10)
+                                    ),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 10, right: 10),
+                                    child: Text(
+                                      widget.destName,
+                                      style:  TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w400,
+                                        height: 0
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 20, right:20),
+                  child: Text(
+                    "Options",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Urbanist',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 30, bottom: 40),
+                  child: Center(child: CircularProgressIndicator()),
+                )
+              ],
             );
           } else if (journeyload.hasData) {
             final journeys = journeyload.data!;
