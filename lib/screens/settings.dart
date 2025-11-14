7cf0c6ddbc7b13b7cacee5e3052b538852f635d7
Static <puppy2655@gmail.com>
2025-11-14 00:32:25 -0500

    dark mode toggle in the settings menu!

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -15,15 +15,6 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     ThemeProvider themeProvider = Provider.of<ThemeProvider>(context, listen: false);
-    
-    // Switch(
-    //   value: themeProvider.theme == ThemeStyle.dark,
-    //   onChanged: (newVal) {
-    //     setState(() {
-    //       themeProvider.swap();
-    //     });
-    //   },
-    // ),
 
     // only a Scaffold here to allow the use of Switch within it
     // Otherwise, we could create our own decorated switch widget to use
@@ -34,9 +25,10 @@ class _SettingsState extends State<Settings> {
           padding: const EdgeInsets.only(
             left: 25,
             right: 15,
-            top: 10
+            top: 15
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               // Settings title and x button
               Row(
@@ -60,6 +52,48 @@ class _SettingsState extends State<Settings> {
                   ),
                 ],
               ),
+
+              const SizedBox(height: 20),
+
+              const Text(
+                'Appearance',
+                style: TextStyle(
+                  fontFamily: 'Urbanist',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                ),
+                textAlign: TextAlign.left,
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 5
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Dark mode',
+                      style: TextStyle(
+                        fontFamily: 'Urbanist',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 18,
+                      ),
+                    ),
+
+                    Switch(
+                      value: themeProvider.theme == ThemeStyle.dark,
+                      onChanged: (newVal) {
+                        setState(() {
+                          themeProvider.swap();
+                        });
+                      },
+                    ),
+                  ],
+                )
+              )
             ],
           ),
         )
