43858d40041ff96a932d8aabd63d5b62edf0e2a5
TomAndTheCats <64748532+Nnanyere@users.noreply.github.com>
2026-04-08 21:02:58 -0400

    fix/refactor(widget) Fixed multiple lints
    Fixed the prefer_conditional_assignment, prefer_is_empty, and use_build_context_synchronously lints in stop_sheet.dart

--- lib/widgets/stop_sheet.dart
+++ lib/widgets/stop_sheet.dart
@@ -324,9 +324,7 @@ class _StopSheetState extends State<StopSheet> {
               arrivingBuses.sort(
                 (lhs, rhs) => (int.tryParse(lhs.prediction) ?? 0).compareTo(int.tryParse(rhs.prediction) ?? 0)
               );
-              if (_isFavorited == null) {
-                _isFavorited = snapshot.data!.$2;
-              }
+              _isFavorited ??= snapshot.data!.$2;
             }
 
             double initialSize = 0.9;
@@ -543,7 +541,6 @@ class _StopSheetState extends State<StopSheet> {
                                                       crossAxisAlignment:
                                                           CrossAxisAlignment.start,
                                                       children: [
-                                                        (arrivingBuses.length == 0)
                                                             ?
                                                               Center(
                                                                 child: Column(
